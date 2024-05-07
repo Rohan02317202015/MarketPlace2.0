07b7cadeb0a0ab5a2b9e3f853f323af54c95833d
shubham <kardamshubhamkumar@gmail.com>
2024-05-07 17:18:28 +0530

    push check

--- app/src/main/java/com/henry/marketplace20/MainActivity.kt
+++ app/src/main/java/com/henry/marketplace20/MainActivity.kt
@@ -44,6 +44,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MarketPlace20Theme {
-        Greeting("Android")
+        Greeting("Android Test")
     }
 }
